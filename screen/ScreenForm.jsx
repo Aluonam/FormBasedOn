70114b3c0566e0440083ea8117eb282d040a2c45
Paula Cano <paulacanoroman@gmail.com>
2024-01-05 22:19:52 +0100

    feat: import in screen all component

--- screen/ScreenForm.jsx
+++ screen/ScreenForm.jsx
@@ -1,8 +1,16 @@
+import DiscountLines from '@/components/discountLines/DiscountLines'
+import GeneralData from '@/components/generalData/GeneralData'
+import HeaderTitle from '@/components/headerTitle/HeaderTitle'
+import ProductPartnership from '@/components/productPartnership/ProductPartnership'
 import React from 'react'
 
 const ScreenForm = () => {
   return (
-    <div>ScreenForm
+    <div>
+      <HeaderTitle></HeaderTitle>
+      <GeneralData></GeneralData>
+      <DiscountLines></DiscountLines>
+      <ProductPartnership></ProductPartnership>
     </div>
   )
 }
