92c3547c0c7025763865881704c4c00bbf92decb
Paula Cano <paulacanoroman@gmail.com>
2024-01-05 22:17:21 +0100

    feat: folder discountLines jsx and module.css

---
+++ components/discountLines/DiscountLines.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './DiscountLines.module.css'
+
+const DiscountLines = () => {
+  return (
+    <div>DiscountLines</div>
+  )
+}
+
+export default DiscountLines
