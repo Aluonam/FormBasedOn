442aab224e5c26b090d3bcf66b5821749dc4379c
Paula Cano <paulacanoroman@gmail.com>
2024-01-05 22:18:35 +0100

    feat: folder productPartnership and css

---
+++ components/productPartnership/ProductPartnership.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './ProductPartnership.module.css'
+
+const ProductPartnership = () => {
+  return (
+    <div>ProductPartnership</div>
+  )
+}
+
+export default ProductPartnership
