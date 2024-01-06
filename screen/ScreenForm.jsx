5f84a4f12acf465a263ee5737962aa78d67b86cb
Paula Cano <paulacanoroman@gmail.com>
2024-01-06 11:12:07 +0100

    feat: screen style general padding

--- screen/ScreenForm.jsx
+++ screen/ScreenForm.jsx
@@ -3,10 +3,11 @@ import GeneralData from '@/components/generalData/GeneralData'
 import HeaderTitle from '@/components/headerTitle/HeaderTitle'
 import ProductPartnership from '@/components/productPartnership/ProductPartnership'
 import React from 'react'
+import style from './ScreenForm.module.css'
 
 const ScreenForm = () => {
   return (
-    <div>
+    <div className={style.screenFormGlobalContainer}>
       <HeaderTitle></HeaderTitle>
       <GeneralData></GeneralData>
       <DiscountLines></DiscountLines>
