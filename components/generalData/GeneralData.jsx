24a1ce62ccdf85a6f8784d8c5c9561ccf18de06b
Paula Cano <paulacanoroman@gmail.com>
2024-01-11 21:04:29 +0100

    feaT: install, import and skeleton useForm

--- components/generalData/GeneralData.jsx
+++ components/generalData/GeneralData.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import style from './GeneralData.module.css'
+import { useForm } from 'react-hook-form';
 
 const GeneralData = () => {
+
+  const { register, handleSubmit } = useForm();
+
   return (
-    <div className={style.generalDataContainer}>GeneralData
+    <div className={style.generalDataContainer}>
+      
+      <form>
+
+
+      </form>
+
     </div>
   )
 }
